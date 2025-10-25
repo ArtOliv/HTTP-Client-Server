4b956cd6183e7d7a2870d47415cc85abc30f304b
ArtOliv <arthurcvo1@gmail.com>
2025-10-25 20:47:14 -0300

    Adding comments

--- Client/meunavegador.c
+++ Client/meunavegador.c
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 
 int main(){
+    // Create client socket
     int client_sock = socket(PF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in addrPort;
@@ -12,6 +13,7 @@ int main(){
     addrPort.sin_addr.s_addr = htonl(INADDR_ANY);
     addrPort.sin_port = htons(6969);
 
+    // Connect to a server
     if(connect(client_sock, (struct sockaddr*) &addrPort, sizeof(addrPort)) == -1){
         printf("\nConnect failed\n\n");
         close(client_sock);
@@ -20,6 +22,7 @@ int main(){
         printf("\nConnected to server via Port 6969\n\n");
     }
 
+    // Define client request
     char request[] = 
     "GET / HTTP/1.1\r\n"
     "Host: 127.0.0.1:6969\r\n"
@@ -29,13 +32,14 @@ int main(){
     size_t request_len = strlen(request);
     int received, sent;
 
+    // Client send the request
     sent = send(client_sock, request, request_len, 0);
     if(sent == -1){
         printf("\nSend message failed\n\n");
     }
     
+    // Client receive server response
     char buffer[30000];
-    
     received = recv(client_sock, buffer, sizeof(buffer)-1, 0);
     if(received == 0){
         printf("\nServer connection closed\n\n");
@@ -46,6 +50,7 @@ int main(){
     buffer[received] = '\0';
     printf("%s\n\n", buffer);
 
+    // Close client connection
     close(client_sock);
     printf("\nSocket connection closed\n");
 
