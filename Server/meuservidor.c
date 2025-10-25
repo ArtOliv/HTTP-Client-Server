558795531156d60d27fe2e4de38b5cab39d8d426
ArtOliv <arthurcvo1@gmail.com>
2025-10-25 16:53:11 -0300

    Basic HTTP server

--- Server/meuservidor.c
+++ Server/meuservidor.c
@@ -5,29 +5,33 @@
 #include <arpa/inet.h>
 
 int main(){
-    int server_sock = socket(PF_INET, SOCK_STREAM, 0);
+    // Create server socket
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in addrPort;
     addrPort.sin_family = AF_INET;
     addrPort.sin_addr.s_addr = htonl(INADDR_ANY);
     addrPort.sin_port = htons(6969);
 
+    // Bind server to the port
     if(bind(server_sock, (struct sockaddr*) &addrPort, sizeof(addrPort)) == -1){
-        printf("\nBind Failed to Port 6969\n\n");
+        printf("\nBind failed to port 6969\n\n");
         close(server_sock);
         return 1;
     } else {
-        printf("\nBind Success to Port 6969\n\n");
+        printf("\nBind success to port 6969\n\n");
     }
 
+    // Puts server to listen to a connection
     if(listen(server_sock, 10) == -1){
-        printf("Listen Faled\n\n");
+        printf("Listen failed\n\n");
         close(server_sock);
         return 1;
     } else {
         printf("Listening...\n\n");
     }
 
+    // Create connection socket
     struct sockaddr_in client_addr;
     socklen_t addr_len = sizeof(client_addr);
     
@@ -39,47 +43,50 @@ int main(){
     }
     printf("Accepted a connection\n\n");
 
-    char msg[256];
-    int countR;
+    // Define server response
+    char response[30000];
+    char body[] = "<html><body><h1>Hello World!</h1></body></html>";
+    
+    snprintf(response, sizeof(response), 
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Length: %zu\r\n"
+        "Content-Type: text/html\r\n"
+        //"Connection: close\r\n"
+        "\r\n"
+        "%s", 
+        strlen(body), body
+    );
 
+    size_t response_len  = strlen(response);
+
+    char buffer[30000];
+    int received, sent;
+    
     while(1){
-        printf("To Client: ");
-        fflush(stdout);
-
-        if(!fgets(msg, sizeof(msg), stdin)){
+        received = recv(connection_sock, buffer, sizeof(buffer)-1, 0);
+        if(received == 0){
+            printf("\nClient connection closed\n\n");
+            break;
+        } else if(received == -1){
+            printf("\nReceive message failed\n\n");
             break;
         }
+        buffer[received] = '\0';
+        printf("%s\n", buffer);
 
-        size_t to_send = strlen(msg);
-        if(to_send > 0){
-            int sent = send(connection_sock, msg, to_send, 0);
-            if(sent == -1){
-                printf("\nSend message Failed\n\n");
-                break;
-            }
-
-            if(strcmp(msg, "Close\n") == 0){
-                break;
-            }
-
-            countR = recv(connection_sock, msg, sizeof(msg)-1, 0);
-            if(countR == 0){
-                printf("\nClient connection Closed\n\n");
-                break;
-            } else if(countR == -1){
-                printf("\nReceive message failed\n\n");
-                break;
-            }
-
-            msg[countR] = '\0';
-            printf("\nFrom Client: %s\n", msg);
+        sent = send(connection_sock, response, response_len, 0);
+        if(sent == -1){
+            printf("\nSend message failed\n\n");
+            break;
         }
+        printf("%s\n\n", response);
     }
 
+    // Close server connections
     close(server_sock);
     close(connection_sock);
 
-    printf("\nSocket connection Closed\n");
+    printf("Socket connection closed\n");
 
     return 0;
 }
